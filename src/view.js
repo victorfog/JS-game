da8a3b0311e64aa91167fb285e235674c4e69a67
Victor Danilenko <victor.fog@gmail.com>
2019-10-07 08:49:16 +0300

    browser crash after add view playfild

---
+++ src/view.js
@@ -0,0 +1,38 @@
+export default class View {
+    constructor (element, width, height, rows, collums) {
+        this.element = element;
+        this.width = width;
+        this.height = height;
+
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.context = this.canvas.getContext('2d');
+
+        this.blockWidth = this.width / collums;
+        this.blockHeight  = this.height / rows;
+
+        this.element.appendChild(this.canvas);
+
+    }
+    renderPlayfild(playfild){
+        for (let y = 0; y < playfild.length; y ++){
+            const line = playfild[y];
+
+            for (let x = 0; line.length; x ++ ){
+                const block = line[x];
+
+                if (block){
+                    this.context.fillStyle = 'red';
+                    this.context.strokeStyle = 'black';
+                    this.context.lineWidth = 2;
+
+                    this.context.fillRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight);
+                }
+
+            }
+        }
+    }
+
+
+}
